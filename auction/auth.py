684197897de458e425910b35f65245e894edc4fc
bwen1 <bwen1@live.com>
2020-10-29 00:24:04 +1000

    changed auth

--- auction/auth.py
+++ auction/auth.py
@@ -37,3 +37,30 @@ def authenticate():  # view function
         else:
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def authenticate():  # view function
+    print('In Register View function')
+    registration_form = RegistrationForm()
+    error = None
+    if(registration_form.validate_on_submit() == True):
+        user_name = registration_form.user_name.data
+        password = registration_form.password.data
+        u1 = User.query.filter_by(name=user_name).first()
+        if u1 is None:
+            error = 'Incorrect user name'
+        # takes the hash and password
+        elif not check_password_hash(u1.password_hash, password):
+            error = 'Incorrect password'
+        if error is None:
+            login_user(u1)
+            # this gives the url from where the login page was accessed
+            nextp = request.args.get('next')
+            print(nextp)
+            if next is None or not nextp.startswith('/'):
+                return redirect(url_for('index'))
+            return redirect(nextp)
+        else:
+            flash(error)
+    return render_template('user.html', form=registration_form, heading='Login')
